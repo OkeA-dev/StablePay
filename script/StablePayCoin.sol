43246106b2738b968ad855bfed683e80595f72a0
Oke.A <Okea.dev@gmail.com>
2024-07-14 13:22:25 +0100

    StablePay coin

--- script/StablePayCoin.sol
+++ script/StablePayCoin.sol
@@ -23,6 +23,18 @@
 
 pragma solidity ^0.8.19;
 
+/*
+ * @title StablePayCoin
+ * @author Oke Abdulquadri
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+* This is the contract meant to be owned by SPCEngine. It is a ERC20 token that can be minted and burned by the
+SPCEngine smart contract.
+ */
+
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -33,17 +45,6 @@ contract StablePayCoin is ERC20Burnable, Ownable {
 
     constructor () ERC20("StablePay Coin", "SPC") Ownable(msg.sender) {}
 
-    function burn(uint256 _amount) public override onlyOwner{
-        uint256 balance = balanceOf(msg.sender);
-        if (_amount <= 0) {
-            revert StablePayCoin__MustBeMoreThanZero();
-        }
-        if (_amount > balance) {
-            revert StablePayCoin__BurnAmountExceedsBalance();
-        }
-        super.burn(_amount);
-    }
-
     function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         if (_to == address(0)) {
             revert StablePayCoin__NotZeroAddress();
@@ -53,6 +54,17 @@ contract StablePayCoin is ERC20Burnable, Ownable {
         }
         _mint(_to, _amount);
         return true;
+    }
+
+    function burn(uint256 _amount) public override onlyOwner{
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert StablePayCoin__MustBeMoreThanZero();
+        }
+        if (_amount > balance) {
+            revert StablePayCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
     }
 
 }
