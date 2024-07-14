732b3c4ac84dd9c25afe552c5e2d09a2dd9e3520
Oke.A <Okea.dev@gmail.com>
2024-07-14 08:57:43 +0100

    StablePay token

---
+++ script/StablePayCoin.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.19;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StablePayCoin is ERC20Burnable, Ownable {
+    error StablePayCoin__BurnAmountExceedsBalance();
+    error StablePayCoin__MustBeMoreThanZero();
+    error StablePayCoin__NotZeroAddress();
+
+    constructor () ERC20("StablePay Coin", "SPC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner{
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert StablePayCoin__MustBeMoreThanZero();
+        }
+        if (_amount > balance) {
+            revert StablePayCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert StablePayCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert StablePayCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+
+}
