5a6fb70e71787f9b80aac16a63bd063f5b94a466
Oke.A <Okea.dev@gmail.com>
2024-07-18 14:44:13 +0100

    StablePay Coin

--- script/DeploySPC.s.sol
+++ script/DeploySPC.s.sol
@@ -26,14 +26,27 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {StablePayCoin} from "../src/StablePayCoin.sol";
 import {SPCEngine} from "../src/SPCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeploySPC is Script {
-    StablePayCoin stablePayCoin;
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
 
     function run() external returns (StablePayCoin, SPCEngine) {
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address wbtc, address weth,) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
         vm.startBroadcast();
-        stablePayCoin = new StablePayCoin();
+        StablePayCoin spc = new StablePayCoin();
+        SPCEngine spcEngine = new SPCEngine(tokenAddresses, priceFeedAddresses, address(spc));
+        spc.transferOwnership(address(spcEngine));
+
         vm.stopBroadcast();
 
-           }
+        return (spc, spcEngine);
+    }
 }
